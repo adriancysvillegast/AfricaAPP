d22ce4ae418d1b0d9e48980f5e7fae69f458ada3
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-05-09 19:36:14 -0400

    Initial Commit

---
+++ AfricaApp/AfricaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAppApp.swift
+//  AfricaApp
+//
+//  Created by Adriancys Jesus Villegas Toro on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
